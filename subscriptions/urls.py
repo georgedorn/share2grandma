14591f06df155af92a701997e58aec81963dcf25
mchllweeks <michelle.schuknecht@gmail.com>
2013-05-01 16:57:13 -0700

    working on detail view, half done

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from .views import TumblrSubscriptionCreateView, SubscriptionDetailView, GenericSubscriptionListView
+from .views import TumblrSubscriptionCreateView, SubscriptionDetailView, GenericSubscriptionListView, \
+    SubscriptionDeleteView
 
 urlpatterns = patterns('',
     (r'^create/tumblr', TumblrSubscriptionCreateView.as_view()),
     (r'^list', GenericSubscriptionListView.as_view()),
     (r'^detail/tumblr/(?P<pk>\d+)/$', SubscriptionDetailView.as_view()),
+    (r'^delete/tumblr/(?P<pk>\d+)/$', SubscriptionDeleteView.as_view()),
 )
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, CreateView, DetailView
+from django.views.generic import ListView, TemplateView, CreateView, DetailView, DeleteView
 
 from .models import TumblrSubscription, TumblrSubscriptionForm, GenericSubscription
 
@@ -14,8 +14,10 @@ class SubscriptionDetailView(DetailView):
     model = TumblrSubscription
 
 
-
 class GenericSubscriptionListView(ListView):
     queryset = GenericSubscription.objects.all()
 #    queryset = Book.objects.filter(publisher__name="Acme Publishing")
 #    template_name = "books/acme_list.html"
+
+class SubscriptionDeleteView(DeleteView):
+    model = TumblrSubscription
