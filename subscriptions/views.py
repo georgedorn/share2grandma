4d2a5fa62c2edb01add53e6b279fa2f128b86ad8
Gordon Morehouse <gordon@morehouse.me>
2013-05-27 19:45:31 -0700

    50622695: start i18n

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, TemplateView, CreateView, DetailView, DeleteView
+from django.views.generic import ListView, CreateView, DetailView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 
 from .models import TumblrSubscription, TumblrSubscriptionForm, GenericSubscription
