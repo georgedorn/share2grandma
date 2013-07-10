27cb494e32b49b7cb5635bbebe55d1f379ff89d0
Sam Thompson <georgedorn@gmail.com>
2013-07-09 18:40:50 -0700

    Initial Vacation creation page

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from .views import TumblrSubscriptionCreateView, SubscriptionDetailView, GenericSubscriptionListView, \
-    SubscriptionDeleteView, RecipientCreateView, RecipientDetailView
+    SubscriptionDeleteView, RecipientCreateView, RecipientDetailView, VacationCreateView
 
 urlpatterns = patterns('',
     url(r'^create/tumblr', TumblrSubscriptionCreateView.as_view(), name='subscription_create_tumblr'),
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     url(r'^delete/tumblr/(?P<pk>\d+)/$', SubscriptionDeleteView.as_view(), name='subscription_delete_tumblr'),
     url(r'^recipient/create', RecipientCreateView.as_view(), name='recipient_create'),
     url(r'^recipient/(?P<pk>\d+)/$', RecipientDetailView.as_view(), name='recipient_detail'),
-
+    url(r'^vacation/create/(?P<recipient_id>\d+)/$', VacationCreateView.as_view(), name='vacation_create'),
 )
