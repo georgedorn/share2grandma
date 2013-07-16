127f08783e52e7232eef2ba759b3b6f3e79d8f84
Gordon Morehouse <gordon@morehouse.me>
2013-07-16 09:35:41 -0700

    checkpoint

--- subscriptions/generic_subscription_processor.py
+++ subscriptions/generic_subscription_processor.py
@@ -7,28 +7,36 @@ class GenericSubscriptionProcessor(object):
     def setup_subscription(self):
         """
         Run when the user first sets up this subscription.
+
+        Return: dict of crap expected by convention by caller, or None.
         """
-        raise NotImplementedError
+        return None
 
 
     def pull_content(self):
         """
         Pulls data from the subscription and stores it in this object
+
+        Return: int Number of items stored, total
         """
-        raise NotImplementedError
+        return 0
 
 
     def transform_content_longform(self):
         """
-        Transforms pulled data to a format we like, so we can send it.
-        returns string?
+        Transforms pulled data to a long, full-email format we like, so we can send it.
+
+        Returns a list of strings, one for each object that should be dispatched, or None.
         """
-        raise NotImplementedError
+        return None
 
 
     def transform_content_shortform(self):
         """
-        If implemented, returns a short bit of content to add to
-        Daily Wakeup emails.
+        Transforms pulled data to a short format we can add to DailyWakeups.
+
+        Returns a list of strings, or None.  Default is to return None (i.e., you must
+        override this in subclasses or they won't contribute to DailyWakeup -
+        and that's pretty common - this will be rarely overridden.)
         """
-        raise NotImplementedError
+        return None
