861fc1f91c7f2e9993f5171368cbfe5c69ac60ff
mchllweeks <michelle.schuknecht@gmail.com>
2013-07-29 07:40:54 -0700

    added login required test for dashboard

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -11,3 +11,18 @@ class URLRedirectTest(TestCase):
 
         self.assertTrue(reverse('dashboard_main') in redir[0])
         self.assertEqual(redir[1], 301)
+
+class LoginRequired(TestCase):
+    def test_login_require_dashboard(self):
+
+        pages = [reverse('dashboard_main'), reverse('dashboard_faq')]
+
+        for page in pages:
+            res = self.client.get(page, follow=True)
+
+            success = False
+            for url, status in res.redirect_chain:
+                if reverse('auth_login') in url:
+                    if status >= 301 and status <= 302:
+                        success = True
+            self.assertTrue(success)
