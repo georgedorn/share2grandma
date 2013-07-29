d0c9fa9b08c7678b3a3648c5a44e3aec0498f548
Sam Thompson <georgedorn@gmail.com>
2013-07-28 20:37:06 -0700

    Vacations: Permissions, timezones, more tests.
    Set the start/end timezones in VacationForm.clean(), long before the model sees it.

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.admin.widgets import AdminDateWidget
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
 from .models import TumblrSubscription, Vacation, Recipient
 
@@ -16,9 +17,27 @@ class RecipientForm(forms.ModelForm):
         fields = ('user', 'sender_name', 'sender_phone', 'name', 'email', 'timezone')
 
 class VacationForm(forms.ModelForm):
-    start_date = forms.DateField(widget=AdminDateWidget())
-    end_date = forms.DateField(widget=AdminDateWidget())
+    start_date = forms.DateTimeField(widget=AdminDateWidget())
+    end_date = forms.DateTimeField(widget=AdminDateWidget())
 
     class Meta:
         model = Vacation
         fields = ['start_date', 'end_date']
+
+    def __init__(self, recipient, *args, **kwargs):
+        self.recipient = recipient
+        return super(VacationForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super(VacationForm, self).clean()
+        start_date = cleaned_data.get('start_date')
+        start_date = start_date.replace(tzinfo=self.recipient.timezone)
+        end_date = cleaned_data.get('end_date')
+        end_date = end_date.replace(tzinfo=self.recipient.timezone)
+        if end_date < start_date:
+            raise forms.ValidationError("Vacations must start before they end.")
+        cleaned_data['start_date'] = start_date
+        cleaned_data['end_date'] = end_date
+        return cleaned_data
+    
+    
