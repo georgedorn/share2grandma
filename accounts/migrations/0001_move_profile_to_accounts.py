e30c2deaa6cb84eb1ea99a8de75cb50c71751a54
Sam Thompson <georgedorn@gmail.com>
2013-09-23 18:53:47 -0700

    Updating migration for accounts.models.Profile to work with tests.

--- accounts/migrations/0001_move_profile_to_accounts.py
+++ accounts/migrations/0001_move_profile_to_accounts.py
@@ -3,16 +3,99 @@ import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
+from django.db.utils import DatabaseError
+import sys, os
+devnull = open(os.devnull, 'w')
 
+class RedirectStdStreams(object):
+    def __init__(self, stdout=None, stderr=None):
+        self._stdout = stdout or sys.stdout
+        self._stderr = stderr or sys.stderr
+
+    def __enter__(self):
+        self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
+        self.old_stdout.flush(); self.old_stderr.flush()
+        sys.stdout, sys.stderr = self._stdout, self._stderr
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._stdout.flush(); self._stderr.flush()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Moving Profile from subscriptions to accounts. 
-        db.rename_table('subscriptions_profile', 'accounts_profile')                                                                                                                        
+        # Or, if it doesn't exist due to order of events, just create it.
+        try:
+            with RedirectStdStreams(stdout=devnull, stderr=devnull):
+                db.rename_table('subscriptions_profile', 'accounts_profile')                                                                                                                        
+        except DatabaseError:
+            # Adding model 'Profile'
+            db.create_table(u'accounts_profile', (
+                (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+                ('user', self.gf('django.db.models.fields.related.OneToOneField')(related_name='s2g_profile', unique=True, to=orm['auth.User'])),
+                ('s2g_email', self.gf('django.db.models.fields.EmailField')(max_length=75, null=True)),
+            ))
         db.send_create_signal('accounts', ['Profile'])
 
     def backwards(self, orm):
         # Moving Profile from accounts to subscriptions    
-        db.rename_table('accounts_profile', 'subscriptions_profile')                                                                                                                        
+        try:
+            db.rename_table('accounts_profile', 'subscriptions_profile')                                                                                                                        
+        except DatabaseError:
+            db.create_table(u'subscriptions_profile', (
+                (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+                ('user', self.gf('django.db.models.fields.related.OneToOneField')(related_name='s2g_profile', unique=True, to=orm['auth.User'])),
+                ('s2g_email', self.gf('django.db.models.fields.EmailField')(max_length=75, null=True)),
+            ))
         db.send_create_signal('subscriptions', ['Profile'])
+        
+        
+        
+    models = {
+        u'accounts.profile': {
+            'Meta': {'object_name': 'Profile'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            's2g_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'s2g_profile'", 'unique': 'True', 'to': u"orm['auth.User']"})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['accounts']
