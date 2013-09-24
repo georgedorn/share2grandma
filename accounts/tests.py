2011bf4e64d4ad7632b8c4b633747d6b5600c76b
Sam Thompson <georgedorn@gmail.com>
2013-09-23 20:04:32 -0700

    Added tests of auth list page

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,11 +6,53 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from .views import AUTH_SERVICES
+from social_auth.models import SOCIAL_AUTH_MODELS_MODULE
+UserSocialAuth = SOCIAL_AUTH_MODELS_MODULE.UserSocialAuth
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class TestAccountAuthServices(TestCase):
+
+    def setUp(self):
+        self.auth_list_url = reverse('auth_services_list')
+        self.userdata = {'username':'xenuuu',
+                         'password':'test_pass'}
+        self.user = User.objects.create_user(**self.userdata)
+    
+    def test_logged_in_s2g(self):
+        self.client.login(**self.userdata)
+        
+        res = self.client.get(self.auth_list_url)
+        
+        #we're logged in with s2g but nothing else, so all of the other services
+        #should present login links
+        for key in AUTH_SERVICES:
+            if key == 's2g':
+                continue
+            
+            auth_begin_url = reverse('socialauth_begin', kwargs={'backend':key})
+            self.assertTrue(auth_begin_url in res.content)
+            
+
+    def test_logged_in_tumblr(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Fake being logged in via tumlbr, then check the auth list.
         """
-        self.assertEqual(1 + 1, 2)
+        UserSocialAuth.objects.create(user=self.user,
+                                      provider='tumblr',
+                                      uid='monkey')
+        self.client.login(**self.userdata)
+        res = self.client.get(self.auth_list_url)
+
+        #so, at this point the user is logged in with s2g and tumblr (can't actually test just tumblr due to django-social-auth's limitations)
+        for key in AUTH_SERVICES:
+            if key == 'tumblr':
+                expected_url = reverse('socialauth_disconnect', kwargs={'backend':key})
+            elif key == 's2g':
+                expected_url = reverse('django.contrib.auth.views.logout')
+            else:
+                expected_url = reverse('socialauth_begin', kwargs={'backend':key})
+            
+            self.assertTrue(expected_url in res.content)
--- accounts/views.py
+++ accounts/views.py
@@ -3,16 +3,28 @@ from collections import OrderedDict
 from social_auth.db.django_models import UserSocialAuth
 from django.views.generic import ListView
 from braces.views import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
 
 #@todo: move to settings after pull_content_refactor merged.
 AUTH_SERVICES = OrderedDict(
                             [
-                             ('tumblr','Tumblr'),
-                             ('google-oauth2','Google Accounts'),
-                             ('s2g', 'Share2Grandma'),
+                             ('tumblr',{'name':'Tumblr'}),
+                             ('google-oauth2',{'name':'Google Accounts'}),
+                             ('s2g', {'name':'Share2Grandma'}),
                             ]
                            )
 
+AUTH_SERVICES['s2g']['connect_url'] = reverse_lazy('login')
+AUTH_SERVICES['s2g']['disconnect_url'] = reverse_lazy("django.contrib.auth.views.logout")
+
+for key in AUTH_SERVICES:
+    if 'connect_url' not in AUTH_SERVICES[key]:
+        connect_url = reverse_lazy('socialauth_begin', kwargs={'backend':key})
+        AUTH_SERVICES[key]['connect_url'] = connect_url
+    if 'disconnect_url' not in AUTH_SERVICES[key]:
+        disconnect_url = reverse_lazy('socialauth_disconnect', kwargs={'backend':key})
+        AUTH_SERVICES[key]['disconnect_url'] = disconnect_url
+        
 
 class ServicesListView(LoginRequiredMixin, ListView):
 
@@ -28,6 +40,7 @@ class ServicesListView(LoginRequiredMixin, ListView):
         logged_in_accounts = context['object_list']
         logged_in_providers = [service.provider for service in logged_in_accounts]
         connected_services = set()
+
         #User.has_usable_password() returns False if user only registered via social auth services
         if self.request.user.has_usable_password():
             connected_services.add('s2g')
@@ -38,8 +51,6 @@ class ServicesListView(LoginRequiredMixin, ListView):
         context['connected_services'] = connected_services 
         context['all_services'] = AUTH_SERVICES.items()
 
-        print context
-        
         return context 
             
         
