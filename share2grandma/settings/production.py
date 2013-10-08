b8b158b4bacadaecfb1dffb00bcfd85e7ee1356c
Sam Thompson <georgedorn@gmail.com>
2013-10-07 22:15:18 -0700

    Changes to settings and requirements as part of deployment to production

--- share2grandma/settings/production.py
+++ share2grandma/settings/production.py
@@ -7,12 +7,19 @@ from .base import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+ADMINS = (('Devs', 'devs@share2grandma.com'),)
+
+ALLOWED_HOSTS = [
+                'app.share2grandma.com',
+                'taffy.utopiadammit.com' #remove me when this is fixed
+
+]
+
 #todo:  figure this out
 #EMAIL_HOST = "localhost"
 #EMAIL_PORT = 1025
 
-DB_USER = 'grandma_app_user'
-DB_PASS = os.environ['DB_PASS']
+DB_USER = 's2g'
 
 DATABASES = {
     'default': {
@@ -20,10 +27,16 @@ DATABASES = {
         'NAME': 'grandma_app_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': DB_USER,
-        'PASSWORD': DB_PASS,
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5433',                      # Set to empty string for default.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
-
+#These MUST be set in the environment or kaboom.
+SECRET_KEY = os.environ['SECRET_KEY']
+TUMBLR_CONSUMER_KEY = os.environ['TUMBLR_CONSUMER_KEY']
+TUMBLR_API_KEY = TUMBLR_CONSUMER_KEY
+TUMBLR_CONSUMER_SECRET = os.environ['TUMBLR_CONSUMER_SECRET']
+GOOGLE_OAUTH2_CLIENT_ID = os.environ['GOOGLE_OAUTH2_CLIENT_ID']
+GOOGLE_OAUTH2_CLIENT_SECRET = os.environ['GOOGLE_OAUTH2_CLIENT_SECRET']
