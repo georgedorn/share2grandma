0c305c8d8baf32a9b2b395a00b7251a21d98696a
Sam Thompson <georgedorn@gmail.com>
2013-10-12 14:26:56 -0700

    production db name fix

--- share2grandma/settings/production.py
+++ share2grandma/settings/production.py
@@ -24,7 +24,7 @@ DB_USER = 's2g'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'grandma_app_db',                      # Or path to database file if using sqlite3.
+        'NAME': 's2g',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': DB_USER,
         'PASSWORD': '',
