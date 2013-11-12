b846cd6b9ae3e70f3ae1ba2e3fd289581e3be48a
Gordon Morehouse <gordon@morehouse.me>
2013-11-11 21:26:31 -0800

    I added get_bucket and stubbed tests for it. Tests do not pass.

--- project_tests/tests/utils.py
+++ project_tests/tests/utils.py
@@ -14,18 +14,6 @@ def make_now_function(year=2013, month=11, day=12, hour=0, minute=0, second=0):
 utc_fn_name = 'share2grandma.utils.get_current_time_utc'
 
 class UtilsTests(TestCase):
-    
-    def test_get_current_bucket(self):
-        with patch(utc_fn_name, make_now_function(hour=13, minute=48)):
-            bucket = get_current_bucket()
-            self.assertEqual(bucket, 27)
-        
-        with patch(utc_fn_name, make_now_function(hour=23, minute=59)):
-            self.assertEqual(get_current_bucket(), 47)
-
-        with patch(utc_fn_name, make_now_function(hour=0, minute=0)):
-            self.assertEqual(get_current_bucket(), 0)
-            
     def test_get_current_time_utc(self):
         """
         Mostly a test for coverage purposes; get_current_time_utc() is
@@ -38,4 +26,77 @@ class UtilsTests(TestCase):
         self.assertLessEqual(diff.total_seconds(), 2)
         
 
+class GetBucketTests(TestCase):
 
+    def test_get_bucket_ultra_basic_tests(self):
+        with patch(utc_fn_name, make_now_function(hour=13, minute=48)):
+            bucket = get_current_bucket()
+            self.assertEqual(bucket, 27)
+
+        with patch(utc_fn_name, make_now_function(hour=23, minute=59)):
+            self.assertEqual(get_current_bucket(), 47)
+
+        with patch(utc_fn_name, make_now_function(hour=0, minute=0)):
+            self.assertEqual(get_current_bucket(), 0)
+
+
+    def test_get_bucket_summerwinter_non_dst_zones(self):
+        ## Africa/Dar_es_Salaam
+        #self.assertEqual(result, expect,
+        #                 "Expected %d for %s (interpreted as %s), got %d" % (expect, recip.timezone, tz_interp, result))
+        #
+        ## Argentina/Buenos_Aires
+        #self.assertEqual(result, expect,
+        #                 "Expected %d for %s (interpreted as %s), got %d" % (expect, recip.timezone, tz_interp, result))
+        #
+        ## America/Phoenix
+        #self.assertEqual(result, expect,
+        #                 "Expected %d for %s (interpreted as %s), got %d" % (expect, recip.timezone, tz_interp, result))
+        #
+        ## Asia/Saigon
+        #self.assertEqual(result, expect,
+        #                 "Expected %d for %s (interpreted as %s), got %d" % (expect, recip.timezone, tz_interp, result))
+        self.skipTest('writeme')
+
+
+    def test_get_bucket_summerwinter_non_dst_weird_zones(self):
+        #"""
+        #no DST but non-even minutes
+        #"""
+        #
+        ## Asia/Katmandu		+05:45	+05:45
+        #self.assertEqual(result, expect,
+        #                 "Expected %d for %s (interpreted as %s), got %d" % (expect, recip.timezone, tz_interp, result))
+        #
+        ## Asia/Calcutta		+05:30	+05:30
+        #self.assertEqual(result, expect,
+        #                 "Expected %d for %s (interpreted as %s), got %d" % (expect, recip.timezone, tz_interp, result))
+        #
+        ## Pacific/Marquesas
+        #self.assertEqual(result, expect,
+        #                 "Expected %d for %s (interpreted as %s), got %d" % (expect, recip.timezone, tz_interp, result))
+        self.skipTest('writeme')
+
+
+    def test_get_bucket_summerwinter_dst_zones(self):
+        # America/Resolute
+        #June
+        #specified_local_noon_dt = datetime.combine(date(2013, 6, 15), time(12, 0, 0, 0, tzinfo=tz))
+        #recip.__get_local_noon_dt = Mock(return_value=specified_local_noon_dt)
+        #tz_interp = datetime.now(tz=recip.timezone).tzname()
+        #expect = 4
+        #result = recip.localnoon_hour
+        #self.assertEqual(result, expect,
+        #                 "Expected %d for %s in %s (interpreted as %s), got %d" % (expect, specified_local_noon_dt, recip.timezone, tz_interp, result))
+        #
+        ## America/Chicago
+        ## Europe/Copenhagen
+        ## Australia/Hobart
+        self.skipTest('writeme')
+
+
+    def test_get_bucket_summerwinter_dst_weird_zones(self):
+        # DST with non-even minutes
+        # America/St_Johns
+        # Pacific/Chatham
+        self.skipTest('writeme')
--- share2grandma/utils.py
+++ share2grandma/utils.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-
+import sanetime
 
 def get_current_time_utc():
     """
     A wrapper for datetime.utcnow() which can be mocked.
     """
     return datetime.utcnow()
+
 
 def get_current_bucket():
     """
@@ -21,3 +22,22 @@ def get_current_bucket():
         odd_bucket = 1
 
     return (now.hour * 2) + odd_bucket
+
+
+def get_bucket(desired_time, timezone):
+    """
+    @param time a string in HH:MM:SS
+    @param timezone an Olson string (e.g. "America/Los_Angeles" or pytz.tzinfo instance
+    @return int a bucket_t, 0-47, with the bucket on today's date with above time and timezone
+    """
+    desired_dt = sanetime.time(desired_time, tz=timezone)
+    hour = desired_dt.utc_datetime.hour
+    minute = desired_dt.utc_datetime.minute
+
+    bucket = 2 * hour
+    bucket += (minute/30) # adds 1 if minute greater or equal to 30
+
+    return bucket
+
+
+
